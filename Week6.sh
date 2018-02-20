91f485787850910cef6a628ea29fb1ef3fda3f04
A2795 <mussieasrat47@gmail.com>
2018-02-20 21:50:45 +0000

    How to create a linux bootable USB

---
+++ Week6.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Chapter6
+
+echo "How to create a linux bootable USB"
+
+echo "1.Download the Ubuntu ISO file you want to place on the USB drive 
+and the Linux Live USB Creator application"
+
+echo "2.Insert the USB drive you want to use into your computer’s USB 
+port and launch the “LiLi USB Creator” application you just installed"
+
+echo "3.Select the USB drive you want to use in the “Step 1: Choose Your 
+Key” box"
+
+echo "4.Provide your downloaded Ubuntu ISO file. Click the “ISO / IMG / 
+ZIP” button under “Step 2: Choose a Source”, browse to the .ISO file on 
+your computer, and double-click it"
+
+echo "5.Use the options in the “Step 3: Persistence” section to select 
+how much space your want to use for persistent storage on the USB drive. 
+Drag the slider all the way to the right to select the maximum amount of 
+storage"
+
+echo "6.You’ve now configured all the settings you need to configure. To 
+create your live USB drive with persistent storage, click the lightning 
+icon under “Step 5: Create"
+
+echo "7.Give the tool some time to create the drive. When the process is 
+done, you’ll see a “Your LinuxLive key is now up and ready!” message. 
+You can now either reboot your computer and boot from the USB drive or 
+unplug the USB drive"
+
+echo "8. last but not the least To confirm that persistent storage is 
+working properly, boot the USB drive and create a folder on the desktop, 
+or save a file to the desktop. Then, shut down your system and boot the 
+live USB drive again. You should see the folder or file you placed on 
+the desktop"
+
+echo "Source:www.howtogeek.com"
